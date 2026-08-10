b0b41942e41432d24e3288de63fcd68413d942b0
Saron Hailemeskel <saronhailemeskel15@gmail.com>
2026-08-10 20:20:55 +0300

    inital data format

---
+++ data/students.js
@@ -0,0 +1,14 @@
+let students =[
+    {
+  id: 1,
+  name: "Amina Hassan",
+  email: "amina@example.com",
+  age: 21,
+  course: "Computer Science",
+  gpa: 3.7,
+  status: "Active"   // Active | Inactive
+}
+
+];
+
+module.exports= students;
