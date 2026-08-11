79b67df9537b93b7e6e0ab7a2ae7b6ba6d283eae
Saron Hailemeskel <saronhailemeskel15@gmail.com>
2026-08-11 07:26:42 +0300

    add server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+// Import routes
+const studentRoutes = require("./routes/students");
+
+// Import middleware
+const logger = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+
+// 1. Built-in middleware
+app.use(express.json());
+
+// 2. Logger middleware
+app.use(logger);
+
+// 3. Routes
+app.use("/api/students", studentRoutes);
+
+// 4. Error-handling middleware
+app.use(errorHandler);
+
+// 5. Start server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
